7bff9e158a39959777097ebb549117c912e58d84
robdir <rhwickham@gmail.com>
2017-12-06 14:52:20 +0100

    styles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import GameContainer from './components/gameContainer'
 
@@ -8,15 +7,9 @@ class App extends Component {
 
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Hangman in React</h1>
-        </header>
-        <div>
         <p className="App-intro">
         Type a letter to kick things off!
         </p>
-        </div>
         <div>
         <GameContainer />
         </div>
